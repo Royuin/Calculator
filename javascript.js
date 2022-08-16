ed25f091afb9b600869ccb552542c3607a6e0152
Gage <yaujin@protonmail.com>
2022-08-15 20:56:53 -0500

    changed default display value from 0 to '0' and created query selector for button1 with event listener to add1 to display value

--- javascript.js
+++ javascript.js
@@ -18,8 +18,18 @@ function operate(func, a, b) {
   return func(a, b);
 }
 
-let displayValue = 0;
+let displayValue = "0";
 
 const display = document.querySelector(".display");
 
 display.textContent = displayValue;
+
+const btn1 = document.querySelector(".button1");
+btn1.addEventListener("click", () => {
+  if (displayValue === "0") {
+    displayValue = "";
+  }
+
+  displayValue += "1";
+  display.textContent = displayValue;
+});
