18b9c46a5430bf5482f221a7e53d53fc411fb57d
Gage <yaujin@protonmail.com>
2022-09-10 17:23:58 -0500

    made query selector for all num buttons

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ function divide(a, b) {
 function operate(a, func, b) {
   let num = func(a, b);
   let result = num.toString();
-  return (displayValue = result.substring(0, 10));
+  return (displayValue = result.substring(0, 9));
 }
 
 let displayValue = "0";
@@ -40,125 +40,125 @@ clearbtn.addEventListener("click", () => {
   };
 });
 
-const btn0 = document.querySelector(".button0");
-btn0.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn0 = document.querySelector(".button0");
+// btn0.addEventListener("click", () => {
+//   if (displayValue === "0") {
+// displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "0";
-  display.textContent = displayValue;
-});
+//   displayValue += "0";
+//   display.textContent = displayValue;
+// });
 
-const btn1 = document.querySelector(".button1");
-btn1.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn1 = document.querySelector(".button1");
+// btn1.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "1";
-  display.textContent = displayValue;
-});
+//   displayValue += "1";
+//   display.textContent = displayValue;
+// });
 
-const btn2 = document.querySelector(".button2");
-btn2.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn2 = document.querySelector(".button2");
+// btn2.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "2";
-  display.textContent = displayValue;
-});
+//   displayValue += "2";
+//   display.textContent = displayValue;
+// });
 
-const btn3 = document.querySelector(".button3");
-btn3.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn3 = document.querySelector(".button3");
+// btn3.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "3";
-  display.textContent = displayValue;
-});
+//   displayValue += "3";
+//   display.textContent = displayValue;
+// });
 
-const btn4 = document.querySelector(".button4");
-btn4.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn4 = document.querySelector(".button4");
+// btn4.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "4";
-  display.textContent = displayValue;
-});
+//   displayValue += "4";
+//   display.textContent = displayValue;
+// });
 
-const btn5 = document.querySelector(".button5");
-btn5.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn5 = document.querySelector(".button5");
+// btn5.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "5";
-  display.textContent = displayValue;
-});
+//   displayValue += "5";
+//   display.textContent = displayValue;
+// });
 
-const btn6 = document.querySelector(".button6");
-btn6.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn6 = document.querySelector(".button6");
+// btn6.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "6";
-  display.textContent = displayValue;
-});
+//   displayValue += "6";
+//   display.textContent = displayValue;
+// });
 
-const btn7 = document.querySelector(".button7");
-btn7.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn7 = document.querySelector(".button7");
+// btn7.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "7";
-  display.textContent = displayValue;
-});
+//   displayValue += "7";
+//   display.textContent = displayValue;
+// });
 
-const btn8 = document.querySelector(".button8");
-btn8.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn8 = document.querySelector(".button8");
+// btn8.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "8";
-  display.textContent = displayValue;
-});
+//   displayValue += "8";
+//   display.textContent = displayValue;
+// });
 
-const btn9 = document.querySelector(".button9");
-btn9.addEventListener("click", () => {
-  if (displayValue === "0") {
-    displayValue = "";
-  } else if (displayValue.length > 9) {
-    return;
-  }
+// const btn9 = document.querySelector(".button9");
+// btn9.addEventListener("click", () => {
+//   if (displayValue === "0") {
+//     displayValue = "";
+//   } else if (displayValue.length > 9) {
+//     return;
+//   }
 
-  displayValue += "9";
-  display.textContent = displayValue;
-});
+//   displayValue += "9";
+//   display.textContent = displayValue;
+// });
 
 const decimalBtn = document.querySelector(".decimal");
 decimalBtn.addEventListener("click", () => {
@@ -177,7 +177,6 @@ let operation = {
 
 const negativePositiveBtn = document.querySelector(".negative-positive");
 negativePositiveBtn.addEventListener("click", () => {
-  console.log(displayValue.includes("-"));
   if (displayValue === "0") {
     return;
   } else if (displayValue.includes("-") === true) {
@@ -277,3 +276,5 @@ divideButton.addEventListener("click", () => {
     displayValue = "";
   }
 });
+
+const numButtons = document.querySelectorAll(".num-button");
